ccc51dad5d5dd61d31640f2badda1ef8b4a919f3
Fabbians <olorunniyioluwapelumi0417@gmail.com>
2026-09-23 04:25:32 -0700

    Add JavaScript string methods exercise

---
+++ javascript/Day 1/Day-2/assigment/index.js
@@ -0,0 +1,29 @@
+
+let challenge = '30 Days Of JavaScript';
+console.log(challenge);
+console.log(challenge.length);
+console.log(challenge.toUpperCase());
+console.log(challenge.toLowerCase());
+console.log(challenge.substring(3));
+console.log(challenge.slice(3));
+console.log(challenge.includes('Script'));
+console.log(challenge.split(''));
+console.log(challenge.split(' '));
+console.log(
+  'Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon'.split(', '));
+console.log(challenge.replace('JavaScript', 'Python'));
+console.log(challenge.charAt(15));
+console.log(challenge.charCodeAt(challenge.indexOf('J')));
+console.log(challenge.indexOf('a'));
+console.log(challenge.lastIndexOf('a'));
+const sentence =
+  'You cannot end a sentence with because because because is a conjunction';
+console.log(sentence.indexOf('because'));
+console.log(sentence.lastIndexOf('because'));
+console.log(sentence.search('because'));
+console.log(' 30 Days Of JavaScript '.trim());
+console.log(challenge.startsWith('30 Days'));
+console.log(challenge.endsWith('JavaScript'));
+console.log(challenge.match(/a/g));
+console.log('30 Days Of'.concat(' JavaScript'));
+console.log(challenge.repeat(2));
